4a56fbc52867f29b21804f3bfc83a7199704ba19
karthikaminash <karthikaminash@gmail.com>
2016-09-29 08:37:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -55,11 +55,11 @@ return htmlTemplate;
  
 
 app.get('/', function (req, res) {
-  res.send(createTemplate(articleOne));
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one' , function (req, res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createTemplate(articleOne));
 });
 app.get('/article-two' , function (req, res){
     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
