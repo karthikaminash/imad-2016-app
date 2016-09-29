d95b859450bf805819f67b72809a3e4ce5a391d1
karthikaminash <karthikaminash@gmail.com>
2016-09-29 20:25:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,11 +18,21 @@ img.onclick = function() {
 };
 
 var button = document.getElementById("counter");
-var counter = 0;
-button.onclick = function() {
-    counter = counter + 1;
-    var span = document.getElementById("count");
-    span.innerHTML = counter.toString();
-    
-};
 
+button.onclick = function() {
+   var request = new XMLHttpRequest();
+   
+   request.onsteadystatechange = function() {
+       if (request.onsteadystatechange === XMLHttpRequest.DONE) {
+           if (request.status === 200) {
+               var counter = request.responseText;
+             
+              var span = document.getElementById("count");
+              span.innerHTML = counter.toString();
+           }
+       }
+   };  
+   };
+   
+   
+   
