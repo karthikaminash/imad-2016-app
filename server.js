92c00f7f213678f7891e0346ef4559afcd6529f1
karthikaminash <karthikaminash@gmail.com>
2016-09-29 15:25:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -56,6 +56,7 @@ var htmlTemplate =
        <link href="/ui/style.css" rel="stylesheet" />
     </head>
     <body>
+    <a href='/' >Home</a>
         <div class="container">
        
         <h1>
