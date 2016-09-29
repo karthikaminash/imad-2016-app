731e785aa246290e987c7eb665e25fd657174c6c
karthikaminash <karthikaminash@gmail.com>
2016-09-29 11:24:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,7 @@ var marginLeft = 0;
 function moveRight () {
     marginLeft = marginLeft + 10;
     img.style.marginLeft = marginLeft + "px" ;
-    
+    marginLeft = 50;
 }
 
 img.onclick = function() {
