cb18fea0f1344d0c5c0a421fe9a7f95036544d3a
karthikaminash <karthikaminash@gmail.com>
2016-09-29 10:59:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 var element = document.getElementById("main-text");
 
 element.innerHTML = "Hello";
+
+var img = document.getElementById("madi");
+img.onClick = function() {
+  img.style.marginleft = "100px";  
+};
