10486db79b1d21922830b69282b8f5b30e6bd6a0
karthikaminash <karthikaminash@gmail.com>
2016-09-29 08:32:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,12 +1,61 @@
 var express = require('express');
 var morgan = require('morgan');
+
 var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+    title: "ArticleOne",
+    heading: "ArticleOne",
+    date: "28th",
+    content: `<div>
+           
+                this is the content for article one
+          
+        </div>" `
+};
+
+function createTemplate (data) {
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date
+    var content = data.content
+    
+var htmlTemplate = 
+`<html>
+    <head>
+        <title>
+          ${title}
+        </title>
+       <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+       
+        <h1>
+           ${heading}
+        </h1>
+        <hr/>
+        <div> 
+        Date : ${date}
+        </div>
+        <hr/>
+        <div>
+           
+               ${content}
+          
+        </div>
+        </div>
+    </body>
+</html>`;
+return htmlTemplate;
+}
+ 
+
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.send(createTemplate(articleOne));
 });
 
 app.get('/article-one' , function (req, res){
